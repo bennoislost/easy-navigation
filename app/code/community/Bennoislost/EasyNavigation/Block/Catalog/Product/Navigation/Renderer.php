c257aedde36b6ea80c141a79a27e1b122cca561d
Ben McManus <user@server.loc>
2015-10-08 19:45:26 +0100

    add layout handle, include template logic

--- app/code/community/Bennoislost/EasyNavigation/Block/Catalog/Product/Navigation/Renderer.php
+++ app/code/community/Bennoislost/EasyNavigation/Block/Catalog/Product/Navigation/Renderer.php
@@ -3,5 +3,30 @@
 class Bennoislost_EasyNavigation_Block_Catalog_Product_Navigation_Renderer
     extends Mage_Page_Block_Html_Topmenu
 {
+    /**
+     * @param Varien_Data_Tree_Node $menuTree
+     * @param string                $childrenWrapClass
+     *
+     * @return string
+     */
+    protected function _getHtml(
+        Varien_Data_Tree_Node $menuTree,
+        $childrenWrapClass
+    ) {
 
+        $template = $this->getChild('template-selector')
+            ->getTemplatePath(
+                $menuTree,
+                $menuTree->getChildren(),
+                $this->_getMenuTreeDepth($menuTree)
+            );
+
+        return parent::_getHtml($menuTree, $childrenWrapClass);
+    }
+
+    private function _getMenuTreeDepth(Varien_Data_Tree_Node $menuTree)
+    {
+        $depth = $menuTree->getDepth();
+        return is_null($depth) ? 0 : $depth += 1;
+    }
 }
